18b1b25482572e2e35ddc9a3504797442d4dd757
Yurie Hayashi <yurie.hayashi095@gmail.com>
2017-02-09 10:02:09 +0000

    git initやりなおし

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit, :update]
+
+#     user GET    /users/:id(.:format)      users#show  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  # edit_user GET    /users/:id/edit(.:format) users#edit
+  def edit
+    # @user は編集対象のユーザー
+    # current_user はログインしているユーザー    
+    @user = User.find(params[:id])
+    if (current_user != @user)
+      redirect_to root_path
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if (current_user != @user)
+      redirect_to root_path
+    end    
+    if (@user.update(user_profile))
+      redirect_to user_path(@user.id)
+       # OKしょり
+    else
+      flash.now[:alert] = "更新できませんでした"
+      render "edit"
+    end
+    
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                                :password_confirmation)
+  end
+
+  def user_profile
+    params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation, :profile)
+  end                                              
+end
---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+    before_save { self.email= self.email.downcase }
+    validates :name, presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                format: { with: VALID_EMAIL_REGEX },
+                uniqueness: {case_sensitive: false }
+    has_secure_password
+    has_many :microposts
+    has_many :following_relationships, class_name: "Relationship",
+                                        foreign_key: "follower_id",
+                                        dependent: :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
+    has_many :follower_relationships, class_name: "Relationship",
+                                        foreign_key: "followed_id",
+                                        dependent: :destroy
+    has_many :follower_users, through: :follower_relationships, source: :follower
+
+    def follow(other_user)
+        following_relationships.find_or_create_by(followed_id: other_user.id)
+    end
+    
+    def unfollow(other_user)
+        following_relationships = following_relationships.find_by(followed_id: other_user.id)
+        following_relationships.destroy if following_relationship
+    end
+    
+    def following?(other_user)
+        follower_users.inlude?(other_user)
+    end
+    
+end
