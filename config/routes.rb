a0acd98f126c8cab8a6c89b50f36fa5d73cd6ac3
Yurie Hayashi <yurie.hayashi095@gmail.com>
2017-02-15 02:08:35 +0000

    kadaitishutu

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'signup', to:'users#new'
   
+  get 'login', to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+
   resources :users
   resources :microposts
   resources :relationships, only: [:create, :destroy]
