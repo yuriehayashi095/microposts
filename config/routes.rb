02094a74ae226c81a55a7135aeab0730a0f3749a
Yurie Hayashi <yurie.hayashi095@gmail.com>
2017-02-22 08:15:42 +0000

    merge2

--- config/routes.rb
+++ config/routes.rb
@@ -3,31 +3,17 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
   get 'signup', to:'users#new'
-  get    'login' , to: 'sessions#new'
-  post   'login' , to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
   
   get 'login', to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
 
-<<<<<<< HEAD
-  resources :users do 
-    member do
-       get 'followings'
-       get 'followers'
-     end
-   end
-=======
   resources :users do
     member do
       get 'followings'
       get 'followers'
     end
   end
->>>>>>> following-followers
   resources :microposts
   resources :relationships, only: [:create, :destroy]
 end
